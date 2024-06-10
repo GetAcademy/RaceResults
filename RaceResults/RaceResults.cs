0609e67469e8176e3e479315cfe75d1a4c1e8942
GET Academy <terje@kolderup.net>
2024-06-10 14:37:55 +0200

    Initial commit

---
+++ RaceResults/RaceResults.cs
@@ -0,0 +1,41 @@
+﻿using System.Numerics;
+
+namespace RaceResults
+{
+    internal class RaceResults
+    {
+        public readonly List<TimeMeasurement> _timeMeasurements;
+
+        public RaceResults()
+        {
+            var lines = File.ReadAllLines("timedata.csv");
+            _timeMeasurements = new List<TimeMeasurement>();
+            foreach (var line in lines)
+            {
+                var parts = line.Split(',');
+                var bibNumber = int.Parse(parts[0]);
+                var timeMeasurement = GetTimeMeasurement(bibNumber);
+
+
+                //new TimeMeasurement
+                //{
+                //    ,
+                //    TimeAtStart = TimeOnly.Parse(parts[1]),
+
+                //}
+            }
+        }
+
+        public TimeMeasurement GetTimeMeasurement(int bibNumber)
+        {
+            foreach (var tm in _timeMeasurements)
+            {
+                if (tm.BibNumber == bibNumber) return tm;
+            }
+
+            var timeMeasurement = new TimeMeasurement { BibNumber = bibNumber };
+            _timeMeasurements.Add(timeMeasurement);
+            return timeMeasurement;
+        }
+    }
+}
---
+++ RaceResults/TimeMeasurement.cs
@@ -0,0 +1,11 @@
+﻿namespace RaceResults
+{
+    internal class TimeMeasurement
+    {
+        public int BibNumber;
+        public TimeOnly? TimeAtStart;
+        public TimeOnly? TimeAt5k;
+        public TimeOnly? TimeAt10k;
+
+    }
+}
