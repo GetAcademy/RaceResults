ba6909bbafc4a312a45bdd867c4ade14157119a1
GET Academy <terje@kolderup.net>
2024-06-10 15:26:09 +0200

    Update Results.cs

--- RaceResults/Results.cs
+++ RaceResults/Results.cs
@@ -1,4 +1,6 @@
-﻿namespace RaceResults
+﻿using System.Runtime.Intrinsics.X86;
+
+namespace RaceResults
 {
     internal class Results
     {
@@ -23,7 +25,26 @@
 
         public void ShowReport()
         {
-
+            var finished = new List<TimeMeasurement>();
+            foreach (var timeMeasurement in _timeMeasurements)
+            {
+                if (timeMeasurement.TimeAt10k != null)
+                {
+                    finished.Add(timeMeasurement);
+                }
+            }
+            finished.Sort((tmA, tmB) =>
+            {
+                var elapsedA = tmA.TimeAt10k.Value.ToTimeSpan() - tmA.TimeAtStart.Value.ToTimeSpan();
+                var elapsedB = tmB.TimeAt10k.Value.ToTimeSpan() - tmB.TimeAtStart.Value.ToTimeSpan();
+                return Convert.ToInt32(elapsedB.TotalMilliseconds - elapsedA.TotalMilliseconds);
+            });
+            foreach (var timeMeasurement in finished)
+            {
+                var elapsed = timeMeasurement.TimeAt10k.Value.ToTimeSpan()
+                              - timeMeasurement.TimeAtStart.Value.ToTimeSpan();
+                Console.WriteLine(elapsed);
+            }
         }
 
         public TimeMeasurement GetTimeMeasurement(int bibNumber)
